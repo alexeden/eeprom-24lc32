7a596070d305dc443b9b9736838a83ea4dec8389
Alex Eden <alexandereden91@gmail.com>
2022-11-16 22:25:41 -0600

    Example

---
+++ examples/test.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{
+    gpio::GpioExt,
+    i2c::I2c,
+    pac,
+    prelude::*,
+    rcc::{RccExt, SYSCLK_MAX},
+};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Starting");
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    let gpiob = dp.GPIOB.split();
+    let clocks = dp.RCC.constrain().cfgr.sysclk(SYSCLK_MAX.Hz()).freeze();
+    let _delay = cp.SYST.delay(&clocks);
+    let scl = gpiob.pb6.into_alternate_open_drain::<4>();
+    let sda = gpiob.pb7.into_alternate_open_drain::<4>();
+    let _i2c = I2c::new(dp.I2C1, (scl, sda), 400.kHz(), &clocks);
+
+    loop {}
+}
+
+#[panic_handler]
+fn handle_panic(info: &core::panic::PanicInfo) -> ! {
+    rprintln!("PANIC! {}", info);
+    rprintln!("Location {:?}", info.location());
+    if let Some(pl) = info.payload().downcast_ref::<&str>() {
+        rprintln!("Payload {:?}", pl);
+    }
+    loop {}
+}
