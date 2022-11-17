d589b14ff0aff3a45b43b87cfd1905096d0ff9d5
Alex Eden <alexandereden91@gmail.com>
2022-11-16 23:46:11 -0600

    POC for single byte r/w

--- examples/test.rs
+++ examples/test.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+use cortex_m::asm;
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::{
@@ -9,6 +10,8 @@ use stm32f4xx_hal::{
     prelude::*,
     rcc::{RccExt, SYSCLK_MAX},
 };
+
+const DEFAULT_ADDR: u8 = 0x50;
 
 #[entry]
 fn main() -> ! {
@@ -21,8 +24,30 @@ fn main() -> ! {
     let _delay = cp.SYST.delay(&clocks);
     let scl = gpiob.pb6.into_alternate_open_drain::<4>();
     let sda = gpiob.pb7.into_alternate_open_drain::<4>();
-    let _i2c = I2c::new(dp.I2C1, (scl, sda), 400.kHz(), &clocks);
+    let mut i2c = I2c::new(dp.I2C1, (scl, sda), 400.kHz(), &clocks);
+    let mut buffer = [0u8];
 
+    i2c.write_read(DEFAULT_ADDR, &0u8.to_be_bytes(), &mut buffer)
+        .expect("Failed to read");
+    rprintln!("Read {:?}", buffer);
+
+    // let mut write = [0u8; 3];
+    // write[0..2].copy_from_slice(&1u16.to_be_bytes());
+    // write[2] = 0x0A;
+    // rprintln!("Writing this {:#?}", write);
+    // i2c.write(DEFAULT_ADDR, &write).expect("Failed to write");
+    // asm::delay(10_000_000);
+    // rprintln!("Written");
+    i2c.write_read(DEFAULT_ADDR, &1u16.to_be_bytes(), &mut buffer)
+        .expect("Failed to read");
+    rprintln!("Read {:?}", buffer);
+
+    i2c.write_read(DEFAULT_ADDR, &2u16.to_be_bytes(), &mut buffer)
+        .expect("Failed to read");
+    rprintln!("Read {:?}", buffer);
+    i2c.write_read(DEFAULT_ADDR, &3u16.to_be_bytes(), &mut buffer)
+        .expect("Failed to read");
+    rprintln!("Read {:?}", buffer);
     loop {}
 }
 
